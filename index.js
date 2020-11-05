8e9661ef6db988dca010fdeac52c00bc2237b385
AlexXPS <alexsepublic@outlook.com>
2020-11-04 20:14:43 -0500

    great

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-// const request = new Request("http://localhost:8000/cap-builds");
-
-// document.getElementById("submit").addEventListener("click", (event) => {
-//     const id = document.getElementById("index").value;
-//     fetch(request)
-//   .then((response) => response.text())
-//   .then((data) => {
-//     result = JSON.parse(data);
-//     let target;
-//     for (let i = 0; i < result.length; i++) {
-//         if(id == result[i].id) {
-//             target = result[i];
-//             break;
-//         }
-//     }
-
-//     document.getElementById("Name").innerHTML = target.BuildName;
-//     document.getElementById("Shiptype").innerHTML = target.ShipType;
-//     document.getElementById("buildData").innerHTML = target.buildData;
-//   });
-// });
-
-
+document.getElementById("submit").addEventListener("click", (event) => {
+  const id = document.getElementById("index").value;
+  const request = new Request(`http://localhost:8000/cap-builds/${id}`);
+  fetch(request)
+    .then((response) => response.text())
+    .then((data) => {
+      const target = JSON.parse(data);
+      document.getElementById("name").innerHTML = target.BuildName;
+      document.getElementById("shipType").innerHTML = target.ShipType;
+      document.getElementById("data").innerHTML = target.buildData;
+    });
+});
 
 function shipType() {
   // Load the correct file for shipType
   let shipT = document.getElementById("shipType").value;
   let jsonObj;
-  console.log(bbJSON)
+  console.log(bbJSON);
   switch (shipT) {
     case "bb":
       jsonObj = bbJSON;
@@ -52,4 +41,3 @@ function shipType() {
 window.onload = shipType();
 
 document.getElementById("shipType").addEventListener("change", shipType);
-
