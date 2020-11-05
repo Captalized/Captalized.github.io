bd738cbd2b597b38847bd2428940781f69c5c4c0
AlexXPS <alexsepublic@outlook.com>
2020-11-04 19:24:34 -0500

    shipType

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const request = new Request("http://localhost:8000/cap-builds");
+// const request = new Request("http://localhost:8000/cap-builds");
 
-document.getElementById("submit").addEventListener("click", (event) => {
-    const id = document.getElementById("index").value;
-    fetch(request)
-  .then((response) => response.text())
-  .then((data) => {
-    result = JSON.parse(data);
-    let target;
-    for (let i = 0; i < result.length; i++) {
-        if(id == result[i].id) {
-            target = result[i];
-            break;
-        }
+// document.getElementById("submit").addEventListener("click", (event) => {
+//     const id = document.getElementById("index").value;
+//     fetch(request)
+//   .then((response) => response.text())
+//   .then((data) => {
+//     result = JSON.parse(data);
+//     let target;
+//     for (let i = 0; i < result.length; i++) {
+//         if(id == result[i].id) {
+//             target = result[i];
+//             break;
+//         }
+//     }
+
+//     document.getElementById("Name").innerHTML = target.BuildName;
+//     document.getElementById("Shiptype").innerHTML = target.ShipType;
+//     document.getElementById("buildData").innerHTML = target.buildData;
+//   });
+// });
+
+
+
+function shipType() {
+  // Load the correct file for shipType
+  let shipT = document.getElementById("shipType").value;
+  let filepath = "./static/txt/" + shipT + ".json";
+  let json = require(filepath);
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 6; col++) {
+      let selectStr = "[data-row='" + row + "'][data-col='" + col + "']";
+      document.querySelector(selectStr).innerHTML = json[row][col].name;
     }
+  }
+}
 
-    document.getElementById("Name").innerHTML = target.BuildName;
-    document.getElementById("Shiptype").innerHTML = target.ShipType;
-    document.getElementById("buildData").innerHTML = target.buildData;
-  });
-});
+window.onload = shipType();
 
-
+document.getElementById("shipType").addEventListener("change", shipType);
 
